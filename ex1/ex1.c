0feb5a72c9d061d20e3d438293a0902e239b749f
rum-yasuhiro <rum.yasuhiro@gmail.com>
2022-07-17 09:30:35 +0900

    add exercise 1

---
+++ ex1/ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* This is a comment. */
+/* This is a multiple lines of 
+comment I added */
+
+int main(int arcg, char *argv[])
+{
+    int distance = 100;
+    
+    // this is also a comment
+    printf("You are %d miles away\n", distance);
+    
+    return 0;
+}
+
